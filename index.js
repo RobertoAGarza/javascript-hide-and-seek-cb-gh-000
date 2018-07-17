9f65907e24e1af52719a3220ba68389c98928b9d
Roberto Garza <robertg4567@hotmail.com>
2018-07-17 16:13:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  return document.getElementById('app').querySelector('.target')
+}
+
+function increaseRankBy(n){
+  var main = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  for( let i =0; i < main.length; i++) {
+
+      main[i].innerHTML= parseInt(main[i].innerHTML,10) + n
+  }
+}
+
+function deepestChild(){
+  var main = document.getElementById('grand-node').querySelectorAll('div')
+
+  return main[main.length-1]
+}
