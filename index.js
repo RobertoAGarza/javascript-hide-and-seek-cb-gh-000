8a484215b6d8ca8d412270a251ee33b02c6ba6c9
Roberto Garza <robertg4567@hotmail.com>
2018-07-17 15:53:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nestedTarget(){
 function increaseRankBy(n){
   const main = document.getElementById('app').querySelector('.ranked-list').innerHTML
   for( let i =0; i < main.length; i++) {
-    
+
       main[i]= parseInt(main[i],10) + n
   }
 }
