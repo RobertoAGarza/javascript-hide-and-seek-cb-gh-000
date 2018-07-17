f5b622f991fe7ee0dd4a3e301273a0937b83c890
Roberto Garza <robertg4567@hotmail.com>
2018-07-17 15:44:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ function getFirstSelector(selector){
   var element = document.querySelector(selector)
   return element
 }
+
+function nestedTarget(){
+  const main = document.getElementById('app')
+  const nested = main.getElementById('#')
+  const target = nested.target
+  return target
+}
